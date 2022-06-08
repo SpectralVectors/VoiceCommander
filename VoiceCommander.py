729b566fd3d4bdaa98293459031ff15d81d3c332
SpectralVectors <robmuir1@gmail.com>
2022-06-08 10:59:20 -0400

    Switched to dict for commands
    Whenever I see a big block of 'if's I figure there's a better way to do things. I tried to implement a dict with my initial solution, but kept getting errors and unexpected results.
    The solution was to keep everything as a string in the dict - key and value, then simply execute the string at the end of the code.
    Still a very early stage, but this should make it easier to separate the commands list from the actual code if anyone wants to expand on it.
    When I set it up as an addon I will have the commands as a separate file

--- VoiceCommander.py
+++ VoiceCommander.py
@@ -1,30 +1,35 @@
-import bpy
+import bpy, re
 import speech_recognition
 
 recognizer = speech_recognition.Recognizer()
+
+commands = {
+    "delete object":"bpy.ops.object.delete()",
+    "delete objects":"bpy.ops.object.delete()",
+    "add cube": "bpy.ops.mesh.primitive_cube_add()",
+    "add monkey": "bpy.ops.mesh.primitive_monkey_add()",
+    "shade smooth": "bpy.ops.object.shade_smooth()",
+    "subdivide": "bpy.ops.object.modifier_add(type='SUBSURF')",
+    "edit mode": "bpy.ops.object.editmode_toggle()",
+    "object mode": "bpy.ops.object.editmode_toggle()",
+    "select all": "bpy.ops.object.select_all(action='SELECT')",
+    "deselect": "bpy.ops.object.select_all(action='DESELECT')",
+}
 
 with speech_recognition.Microphone() as mic:
     
     audio = recognizer.listen(mic, timeout=1, phrase_time_limit=1)
     text = recognizer.recognize_google(audio)
     text = text.lower()
+    command = commands[text]
     print(f"{text}")
 
-    if text == "delete object":
-        bpy.ops.object.delete()
-    if text == "add cube":
-        bpy.ops.mesh.primitive_cube_add()
-    if text == "add monkey":
-        bpy.ops.mesh.primitive_monkey_add()
-    if text == "shade smooth":
-        bpy.ops.object.shade_smooth()
-    if text == "subdivide":
-        bpy.ops.object.modifier_add(type='SUBSURF')
-    if text == "edit mode":
-        bpy.ops.object.editmode_toggle()
-    if text == "object mode":
-        bpy.ops.object.editmode_toggle()
-    if text == "select all":
-        bpy.ops.object.select_all(action='SELECT')
-    if text == "deselect":
-        bpy.ops.object.select_all(action='DESELECT')
+exec(command)
+        
+        
+# To remove all letters from a string, leaving only
+# numbers and special characters
+# value = re.sub("[A-Za-z]", "", text).strip()
+
+# print(value)
+
